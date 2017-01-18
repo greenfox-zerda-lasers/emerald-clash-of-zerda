53247811db9c51caa6e51b8674aea7d926a043db
brigittaforrai <forraibrigi@gmail.com>
2017-01-18 18:52:33 +0100

    feature(overview): style, elements

--- brigi_experiments/public/data.js
+++ brigi_experiments/public/data.js
@@ -10,6 +10,14 @@ var userData = {
 
 };
 
-var buildingTypes = ["townhall", "mine", "farm", "barracks"];
+var buildingTypes = [
+  {type: "Townhall", image: "https://d30y9cdsu7xlg0.cloudfront.net/png/5841-200.png"},
+  {type: "Mine", image: "https://d30y9cdsu7xlg0.cloudfront.net/png/543-200.png"},
+  {type: "Farm", image: "https://d30y9cdsu7xlg0.cloudfront.net/png/25362-200.png"},
+  {type: "Barracks", image: "https://cdn3.iconfinder.com/data/icons/ahasoft-war/512/barracks-512.png"}
+];
 
-var resourcesTypes = ["gold", "food"];
+var resourcesTypes = [
+  {type: "Food", image: "http://icons.iconarchive.com/icons/icons8/windows-8/128/Food-Doughnut-icon.png"},
+  {type: "Gold", image: "https://cdn1.iconfinder.com/data/icons/jewery/500/gold-512.png"}
+];
