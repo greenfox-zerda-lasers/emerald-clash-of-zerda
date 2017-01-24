2f3d7bbcaaf9230e0e08841df861cce823d729c6
Bencsó <gabor.bencsik13@gmail.com>
2017-01-24 17:34:32 +0100

    feat(buildings): tried to set up a factory

--- public/buildings/buildingsController.js
+++ public/buildings/buildingsController.js
@@ -1,3 +1,9 @@
-angular.module("ClashApp").controller("BuildingsController", ['$scope', '$http', '$location', '$route' , '$localStorage', function($scope, $http, $location, $route, $localStorage){
+angular.module("ClashApp").controller("BuildingsController", ['$scope', '$http', '$location', '$route', '$localStorage', 'BuildingsService', function($scope, $http, $location, $route, $localStorage, BuildingsService){
 
+    console.log("lefut?");
+    $scope.getBuildings = function() {
+      console.log("button works");
+      $scope.buildings = BuildingsService.query();
+      console.log($scope.buildings);
+    };
 }]);
---
+++ public/buildings/buildingsService.js
@@ -0,0 +1,7 @@
+angular.module("ClashApp").factory("BuildingsService", function() {
+// angular.module("ClashApp").factory("BuildingsService", function($resourse) {
+  // var buildings = $resourse('http://localhost:8000/kingdom/:id/buildings/', {id: 0});
+
+  var buildings = "epulet";
+  return buildings;
+})
