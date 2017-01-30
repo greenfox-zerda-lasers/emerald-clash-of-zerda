7dfa0c240cb18ae2b1db35db5dbad3a6dcf9bf16
Bencsó <gabor.bencsik13@gmail.com>
2017-01-30 15:18:21 +0100

    overview refactored, controller rewrite using $resource, minor css and html changes

--- public/overview/overviewController.js
+++ public/overview/overviewController.js
@@ -8,88 +8,50 @@ angular.module("ClashApp").controller("OverviewController", ['$scope', '$http', 
     var getKingdomData = (function () {
       $scope.kingdomData = OverviewService.get()
         .$promise.then( function(result) {
-          $scope.buildings = result.buildinds;
-          $scope.troops2 = result.troops;
+          $scope.buildings = result.buildings;
+          $scope.troops = result.troops;
           $scope.resources = result.resources;
-          console.log(result.buildings);
-          console.log(result.troops);
-          console.log(result.resources);
+          $scope.buildingTypes = getBuildings($scope.buildings);
+          $scope.resourceTypes = getResources($scope.resources);
         });
-      // console.log($scope.kingdomData);
     })();
 
-    console.log($scope.buildings);
-    console.log($scope.kingdomData);
-    console.log($scope.kingdom);
-
-    //BUILDINGS
-    $http({
-      method: 'GET',
-      url: 'http://localhost:8000/kingdom/' + $localStorage.userObj.userId
-    }).then(function(response){
-
-      console.log(response, "buildings")
-      var data = response.data.buildings;
-
-
-      $scope.buildingTypes = [
-        {type: "Townhall", image: "http://image.flaticon.com/icons/svg/288/288698.svg", amount: 0},
-        {type: "Mine", image: "http://image.flaticon.com/icons/svg/234/234785.svg", amount: 0},
-        {type: "Farm", image: "http://image.flaticon.com/icons/svg/284/284493.svg", amount: 0},
-        {type: "Barracks", image: "http://image.flaticon.com/icons/svg/284/284425.svg", amount: 0}
-      ];
-
-      for(var b = 0; b < data.length; b++) {
-        if(data[b].type === "townhall") {
-          $scope.buildingTypes[0].amount += 1;
-        } else if(data[b].type === "mine") {
-          $scope.buildingTypes[1].amount += 1;
-        } else if (data[b].type === "farm") {
-          $scope.buildingTypes[2].amount += 1;
-        } else if (data[b].type === "barrack") { //javitani barracks-ra
-          $scope.buildingTypes[3].amount += 1;
+    var getBuildings = function (data) {
+      var townhall = 0;
+      var mine = 0;
+      var farm = 0;
+      var barrack = 0;
+      data.forEach( function(building) {
+        switch (building.type) {
+          case "townhall":
+            townhall++;
+            break;
+          case "mine":
+            mine++;
+            break;
+          case "farm":
+            farm++;
+            break;
+          case "barrack":
+            barrack++;
+            break;
         };
-      };
-    });
+      });
+      return {townhall, mine, farm, barrack};
+    };
 
-
-    //RESOURCES
-    $http({
-      method: 'GET',
-      url: 'http://localhost:8000/kingdom/' + $localStorage.userObj.userId + '/resources'
-    }).then(function(response){
-      console.log(response, "resources")
-
-      var data = response.data;
-
-      $scope.resourceTypes = [
-        {type: "Food", image: "http://image.flaticon.com/icons/svg/168/168559.svg"},
-        {type: "Gold", image: "http://image.flaticon.com/icons/svg/199/199533.svg"}
-      ];
-
-      for(var r = 0; r < data.length; r++) {
-        if(data[r].type === "food") {
-          $scope.resourceTypes[0].amount = data[r].amount;
-        } else if(data[r].type === "gold") {
-          $scope.resourceTypes[1].amount = data[r].amount;
+    var getResources = function (data) {
+      var food = 0;
+      var gold = 0;
+      data.forEach( function(resource) {
+        switch (resource.type) {
+          case "food":
+            food = resource.amount;
+          case "gold":
+            gold = resource.amount;
         };
-      };
-    });
-
-
-    //TROOPS
-    $http({
-      method: 'GET',
-      url: 'http://localhost:8000/kingdom/' + $localStorage.userObj.userId + '/troops'
-    }).then(function(response){
-      console.log(response, "troops")
-      var data = response.data;
-      $scope.troopsAmount = data.length;
-      $scope.troops = data
-      // console.log($scope.troops, "troops")
-    });
-
+      });
+      return {food, gold};
+    };
   };
-
-
 }]);
