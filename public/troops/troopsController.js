76c8caf86c36c7add364f5cac3d692b35550dca1
brigittaforrai <forraibrigi@gmail.com>
2017-01-30 14:07:00 +0100

    troops layout

--- public/troops/troopsController.js
+++ public/troops/troopsController.js
@@ -1,9 +1,13 @@
-angular.module("ClashApp").controller("TroopsController", ['$scope', '$http', '$location', '$route', '$localStorage', 'TroopsService', function($scope, $http, $location, $route, $localStorage, TroopsService){
+angular
+  .module("ClashApp")
+  .controller("TroopsController", ['$scope', '$http', '$location', '$route', '$localStorage', 'TroopsService',
+  function($scope, $http, $location, $route, $localStorage, TroopsService){
 
   if ($localStorage.userObj.userId === false) {
     $location.path('/login');
   } else {
     $scope.kingdom = $localStorage.userObj.kingdom;
+    $scope.username = $localStorage.userObj.username;
   }
 
 
@@ -11,32 +15,11 @@ angular.module("ClashApp").controller("TroopsController", ['$scope', '$http', '$
   var getTroops = (function() {
     $scope.troops = TroopsService.query();
     console.log($scope.troops, "troops")
+    console.log($scope.troops.$promise.$$state.value)
   })();
 
 
 
-  // $scope.getTroopData = function(id) {
-  //   console.log("click");
-  //   console.log(id);
-  //   $scope.troopResponse = {}
-  //   $scope.troopResponse.open = false;
-  //
-  //   $http({
-  //     method: 'GET',
-  //     url: 'http://localhost:8000/kingdom/' + $localStorage.userObj.userId + '/troops/' + id //2 helyett troopId
-  //   }).then(function(response){
-  //     $scope.troopResponse.id = response.data.troop.id;
-  //     $scope.troopResponse.hp = response.data.troop.hp;
-  //     $scope.troopResponse.attack = response.data.troop.attack;
-  //     $scope.troopResponse.defense = response.data.troop.defense;
-  //     $scope.troopResponse.level = response.data.troop.level;
-  //     //$scope.troopResponse.open = false;
-  //     $scope.troopResponse.open = true
-  //     console.log($scope.troopResponse)
-  //     //$scope.open = true
-  //   });
-  //
-  // };
 
 
 
