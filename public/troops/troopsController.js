e464420a4ac878fbc5b8f08fa886cb0a6a0c5789
brigittaforrai <forraibrigi@gmail.com>
2017-01-31 16:56:08 +0100

    troops upgrade error handling

--- public/troops/troopsController.js
+++ public/troops/troopsController.js
@@ -17,27 +17,27 @@ angular
       });
   })();
 
+  $scope.valami = true;
   $scope.trainTroop = function(id) {
-    console.log(id, "id");
 
-    $scope.troop = TroopsService.get({troopId: id})
-      .$promise.then(function(result) {
-        $scope.troopData = result;
-        //console.log(result, "result")
-        $scope.troopData.level = $scope.troopData.level + 1;
-        //$scope.troops.level = $scope.troopData.level ;
-        
-        TroopsService.update({troopId: id}, {level:$scope.troopData.level})
-          .$promise.then(function(response) {
-           $scope.troops[id] = response;
-        }, function(error) {
-            if(error.status === 400) {
-              $scope.errorMessage = error.data.errors.upgrade
-              $scope.error = true;
-            }
-        }
-      );
+    if($scope.valami) {
+      $scope.troop = TroopsService.get({troopId: id})
+        .$promise.then(function(result) {
+          $scope.troopData = result;
+          $scope.troopData.level = $scope.troopData.level + 1;
+          TroopsService.update({troopId: id}, {level:$scope.troopData.level})
+            .$promise.then(function(response) {
+             $scope.troops[id] = response;
+          }, function(error) {
+              if(error.status === 400) {
+                $scope.errorMessage = error.data.errors.upgrade
+                $scope.error = true;
+                $scope.valami = false
+              }}
+        )})
+    } else {
+      console.log("valamiiii")
+    }
 
-      })
-  }
+    }
 }]);
