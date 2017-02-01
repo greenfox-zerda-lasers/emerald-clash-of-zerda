d5412da619ef2e4f053eaf95f90bc78c1c5782af
Bencsó <gabor.bencsik13@gmail.com>
2017-02-01 15:25:31 +0100

    history factory created

---
+++ public/history/historyFactory.js
@@ -0,0 +1,14 @@
+/**
+ * History service
+ * @desc Handles API of the History
+ * @author Bencso
+ */
+
+angular
+  .module("ClashApp")
+  .factory("HistoryFactory",
+    function($resource, $localStorage) {
+      return $resource('/kingdom/:id/battles/:battle_id/',
+      {id: $localStorage.userObj.userId, battle_id: "@battle_id"}
+    );
+});
