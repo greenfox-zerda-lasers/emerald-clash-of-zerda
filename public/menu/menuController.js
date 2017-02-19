eec04eac90d85ddebec8bf502cb30c3d46129a45
brigittaforrai <forraibrigi@gmail.com>
2017-02-19 15:11:00 +0100

    design and styles wip

--- public/menu/menuController.js
+++ public/menu/menuController.js
@@ -5,35 +5,33 @@ angular
 
   $scope.username = $localStorage.userObj.username;
 
+  let options = document.querySelectorAll(".option")
+  let button = document.querySelector(".activeScreen")
+
   let menuOverlay = (function() {
-    let button = document.querySelector(".activeScreen")
-    let options = document.querySelectorAll(".option")
-    let overlay = document.querySelector(".overlay")
+    let overlay = document.querySelector(".menuLeft")
     button.addEventListener("mouseover", function(event) {
-      // button.style.width = "90px"
-      // button.style.height = "90px"
       options.forEach(function(elem) {
         elem.style.opacity = "1"
-      })
-      overlay.addEventListener("mouseout", function(event) {
-        console.log("out");
-        button.style.width = "75px"
-        button.style.height = "75px"
-        options.forEach(function(elem) {
-          elem.style.opacity = "0"
-        })
+        button.style.width = "100px"
+        button.style.height = "100px"
       })
     })
+    // overlay.addEventListener("mouseout", function(event) {
+    //   console.log("out");
+    //   button.style.width = "70px"
+    //   button.style.height = "70px"
+    //   options.forEach(function(elem) {
+    //     elem.style.opacity = "0"
+    //   })
+    // })
   })()
 
 
-  let options = document.querySelectorAll(".option")
-
   let setScreenPaths = (function() {
-    let screens = ['map', 'overview', 'buildings', 'troops', 'leaderboard', 'history']
-    let button = document.querySelector(".activeScreen")
+    let screens = ['map', 'buildings', 'troops', 'leaderboard', 'history']
     let path = String($location.absUrl())
-    console.log(path);
+
     for(var i = 0; i <screens.length; i++) {
       if(path.includes(screens[i]) ) {
         button.style.backgroundImage = 'url(' + './img/icons/' + screens[i] + '_icon-01.svg' + ')'
@@ -47,6 +45,7 @@ angular
   })()
 
   $scope.goToScreen = function(event){
+    console.log("gotoscreen");
     let screen = event.target.id
     let path = screen.slice(6)
     $location.path('/' + path)
@@ -55,8 +54,8 @@ angular
   $scope.closeMenu = function() {
     options.forEach(function(elem){
       elem.style.opacity = "0"
-      //mouseout-ra ujra elojon! javitani!!
-      //+ window eventlistener
+      button.style.width = "70px"
+      button.style.height = "70px"
     })
   }
 
