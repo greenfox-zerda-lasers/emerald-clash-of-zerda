5e2c9dbdab8f55fff6d3064df37bd097493995c0
Bencsó <gabor.bencsik13@gmail.com>
2017-02-22 19:08:57 +0100

    leaderboard and buildings tests

--- public/leaderboard/leaderboardController.js
+++ public/leaderboard/leaderboardController.js
@@ -1,8 +1,5 @@
 angular.module("ClashApp").controller("LeaderboardController", ['$scope', '$http', '$location', '$route', '$localStorage', 'LeaderboardFactory', function($scope, $http, $location, $route, $localStorage, LeaderboardFactory){
 
-  var getUsers = (function() {
-    $scope.userList = LeaderboardFactory.query();
-    console.log($scope.userList);
-  })();
+  $scope.userList = LeaderboardFactory.query();
 
 }]);
