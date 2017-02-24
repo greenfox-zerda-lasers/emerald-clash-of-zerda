eafc0e8bf1d27f16c54cd9ba443a7d50ddb54dc1
Bencsó <gabor.bencsik13@gmail.com>
2017-02-24 15:40:15 +0100

    minor changes on welcome and controller page

--- public/welcome/welcomeController.js
+++ public/welcome/welcomeController.js
@@ -1,7 +1,7 @@
 angular.module("ClashApp").controller("WelcomeController", ['$scope', '$http', '$localStorage', '$location', '$resource', function($scope, $http, $localStorage, $location, $resource) {
   console.log("welcome from ctrl");
 
-  this.goToGame = function () {
+  $scope.goToGame = function () {
     console.log("map");
     $location.path('/map');
   };
