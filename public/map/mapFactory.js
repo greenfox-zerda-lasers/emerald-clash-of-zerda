562635ab0703380e29ab9dbade8d7ae4cd635480
brigittaforrai <forraibrigi@gmail.com>
2017-02-24 09:25:11 +0100

    map

--- public/map/mapFactory.js
+++ public/map/mapFactory.js
@@ -20,25 +20,4 @@ angular
         )
      }
 
-
-  //atirni ilyenre
-//   .module("ClashApp")
-//   .factory("mapFactory",
-//     function($resource, $localStorage, ConfigFactory) {
-
-//       return {
-//         search: $resource(
-//           `${ConfigFactory.apiURL}search?q=:kingdom`,
-//           {kingdom: "@kingdom"},
-//           {
-//             'update': { method:'PUT' }
-//           }
-//         ),
-//         attack : $resource(
-//           `${ConfigFactory.apiURL}kingdom/:id/attack/`,
-//           {id: $localStorage.userObj.userId}
-//         )
-//       }
-
-
 });
